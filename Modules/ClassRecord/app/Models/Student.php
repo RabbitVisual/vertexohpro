1404a41baadd5f07a48d56c59fbc12974b11a254
Reinan Rodrigues <r.rodriguesjs@gmail.com>
2026-02-07 05:04:02 -0300

    Fix merge conflicts in web.php and Student.php, verify icons.php is clean

--- Modules/ClassRecord/app/Models/Student.php
+++ Modules/ClassRecord/app/Models/Student.php
@@ -9,19 +9,11 @@ class Student extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
     protected $fillable = ['name', 'class_id'];
 
     public function schoolClass()
     {
         return $this->belongsTo(SchoolClass::class, 'class_id');
-=======
-    protected $fillable = ['school_class_id', 'name', 'registration_number'];
-
-    public function schoolClass()
-    {
-        return $this->belongsTo(SchoolClass::class);
->>>>>>> origin/jules/planning-module-init-11986219447505815665
     }
 
     public function attendances()
--- Modules/Library/routes/web.php
+++ Modules/Library/routes/web.php
@@ -4,18 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\Library\Http\Controllers\LibraryController;
 use Modules\Library\Http\Controllers\LibraryResourceController;
 
-<<<<<<< HEAD
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::middleware(['auth', 'verified'])->prefix('library')->name('library.')->group(function () {
     Route::get('/', [LibraryController::class, 'index'])->name('index'); // Marketplace
     Route::get('/my-library', [LibraryController::class, 'myLibrary'])->name('my-library'); // My Library
@@ -27,22 +15,4 @@ Route::middleware(['auth', 'verified'])->prefix('marketplace')->name('marketplac
     Route::view('/success', 'library::success')->name('success');
     Route::view('/failure', 'library::failure')->name('failure');
     Route::view('/pending', 'library::pending')->name('pending');
-=======
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('libraries', LibraryController::class)->names('library');
-    Route::resource('library-resources', LibraryResourceController::class);
-});
-
-use Modules\Library\Http\Controllers\AuthorDashboardController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/author/dashboard', [AuthorDashboardController::class, 'index'])->name('author.dashboard');
-});
-
-use Modules\Library\Http\Controllers\DownloadController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/library/download/{id}', [DownloadController::class, 'download'])->name('library.download');
-    Route::get('/library/stream/{id}', [DownloadController::class, 'stream'])->name('library.stream');
->>>>>>> origin/unify-system-layout-2439224578998115045
 });
